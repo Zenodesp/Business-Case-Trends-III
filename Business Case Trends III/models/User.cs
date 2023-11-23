1f8ff0fc22865756e2315a8095f1260bdbcb4497
Zenodesp <Zeno.despiegelaere@student.ehb.be>
2023-11-23 14:07:25 +0100

    Added user class

---
+++ Business Case Trends III/models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_Case_Trends_III.models
+{
+    internal class User
+    {
+        //generate a number of variables for the user. besides the obvious Id, username and password, we must also need a point counter.   
+        private int id;
+        private string username;
+        private string password;
+        private int points;
+
+
+
+    }
+}
